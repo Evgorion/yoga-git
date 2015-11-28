3948fe49f2e66a3462db240972ccee51fa80ed0f
Evgorion <y071962@gmail.com>
2015-11-28 14:36:38 +0200

    New ver 1.0.2

--- NewFook.java
+++ NewFook.java
@@ -1,5 +1,6 @@
 /** Hello
 @autor Evgeny
 @date 28.11.2015
+@ver 1.0.2
 **/
 print("Program new file");
--- NewTorro.java
+++ NewTorro.java
@@ -1,6 +1,7 @@
 /** Hello
 @autor Evgeny
 @date 28.11.2015
+@ver 1.0.22
 **/
 print("New Torro");
 print("Torro for you");
