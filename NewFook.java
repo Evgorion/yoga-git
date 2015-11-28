2fc7e3f0b6c27a3d3896ceb9a56b0ad1434964ba
Evgorion <y071962@gmail.com>
2015-11-28 14:31:17 +0200

    JavaDoc add

--- NewFook.java
+++ NewFook.java
@@ -1,1 +1,5 @@
+/** Hello
+@autor Evgeny
+@date 28.11.2015
+**/
 print("Program new file");
--- NewTorro.java
+++ NewTorro.java
@@ -1,3 +1,7 @@
+/** Hello
+@autor Evgeny
+@date 28.11.2015
+**/
 print("New Torro");
 print("Torro for you");
 ActionEvent("c:\work\zorro.c");
